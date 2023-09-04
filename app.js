993e6a1b4b7e254e6acd67d29ccbb55e766deb82
Jasper <95355765+Jasper-V-Schneider@users.noreply.github.com>
2023-09-04 21:02:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const button = document.getElementById("submit-button-jasper");
 const list = document.getElementById("list");
 const input = document.getElementById("exampleInputEmail1");
+const radio = document.getElementById("http");
+const radioS = document.getElementById("https");
+let transferProtocol;
 
 function addWebsite() {
+  if (radio.checked) {
+    transferProtocol = "http://";
+  } else if (radioS.checked) {
+    transferProtocol = "https://";
+  } else {
+    alert("Please select a transfer protocol!");
+  }
   if (input.value == "") {
     alert('"" is not a website!');
   } else {
     let li = document.createElement("li");
     let link = document.createElement("a");
-    link.href = "https://"+input.value;
+    link.href = transferProtocol + input.value;
     li.append(link);
     link.innerText = input.value;
     list.appendChild(li);
